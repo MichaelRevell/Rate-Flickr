d589a4f0b0b1779d9bf478be40b12ae60289c61c
Michael <mars8082686@gmail.com>
2012-03-30 17:48:49 -0400

    added rails and bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rateflikr::Application.config.secret_token = 'f5487088b5187037f03123d78f938ece3201dc053600acf41ece73634e8c65e82e59e58e78f7a3997dc0c143441c8b3d39964eb690b8db45a17e2c8b27ac0e16'

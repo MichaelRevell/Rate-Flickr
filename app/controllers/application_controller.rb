6758b2821814109673cac6b7dfcef3c08e8b6a57
Michael <mars8082686@gmail.com>
2012-04-24 18:28:14 -0400

    flicker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,9 @@ class ApplicationController < ActionController::Base
   end
   
   def get_pic(id)
+    
+    FlickRaw.api_key="424ca2d344deb8f86a06ca73f26c9aa8"
+    FlickRaw.shared_secret="0bad88d39b4792c3"
       info = flickr.photos.getInfo :photo_id => id
       @pic = FlickRaw.url_m(info)
       
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,12 @@ class HomeController < ApplicationController
     @image = Image.random
     @imageId = @image.imageId
     @image_id = @image.id
-    @pic = get_pic(@image.imageId)
+    #@pic = get_pic(@image.imageId)
+    
+    FlickRaw.api_key="424ca2d344deb8f86a06ca73f26c9aa8"
+    FlickRaw.shared_secret="0bad88d39b4792c3"
+      info = flickr.photos.getInfo :photo_id => id
+      @pic = FlickRaw.url_m(info)
     
   end
 
